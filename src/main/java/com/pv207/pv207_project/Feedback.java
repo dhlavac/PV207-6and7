8adbffc3b23a6ee2b640ae4054f7cdde9de332fd
wbadmin <>
2020-05-19 14:12:45 +0200

    {/src/main/java/com/pv207/pv207_project/Feedback.java}

--- src/main/java/com/pv207/pv207_project/Feedback.java
+++ src/main/java/com/pv207/pv207_project/Feedback.java
@@ -6,12 +6,35 @@ package com.pv207.pv207_project;
 
 public class Feedback implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Feedback() {
-    }
+	@org.kie.api.definition.type.Label(value = "reviews")
+	private java.lang.String reviews;
+	@org.kie.api.definition.type.Label(value = "stars")
+	private java.lang.Integer stars;
 
+	public Feedback() {
+	}
 
+	public java.lang.String getReviews() {
+		return this.reviews;
+	}
 
+	public void setReviews(java.lang.String reviews) {
+		this.reviews = reviews;
+	}
+
+	public java.lang.Integer getStars() {
+		return this.stars;
+	}
+
+	public void setStars(java.lang.Integer stars) {
+		this.stars = stars;
+	}
+
+	public Feedback(java.lang.String reviews, java.lang.Integer stars) {
+		this.reviews = reviews;
+		this.stars = stars;
+	}
 
 }
