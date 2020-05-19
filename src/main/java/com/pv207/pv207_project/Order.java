9208f786113b2f2ba4830e8b7fdb0e8f48025e23
wbadmin <>
2020-05-19 19:37:26 +0200

    {/src/main/java/com/pv207/pv207_project/Order.java}

--- src/main/java/com/pv207/pv207_project/Order.java
+++ src/main/java/com/pv207/pv207_project/Order.java
@@ -6,12 +6,36 @@ package com.pv207.pv207_project;
 
 public class Order implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Order() {
-    }
+	@org.kie.api.definition.type.Label(value = "sum")
+	private java.lang.Integer sum;
+	@org.kie.api.definition.type.Label(value = "item")
+	private java.util.List<com.pv207.pv207_project.OrderItem> item;
 
+	public Order() {
+	}
 
+	public java.lang.Integer getSum() {
+		return this.sum;
+	}
 
+	public void setSum(java.lang.Integer sum) {
+		this.sum = sum;
+	}
+
+	public java.util.List<com.pv207.pv207_project.OrderItem> getItem() {
+		return this.item;
+	}
+
+	public void setItem(java.util.List<com.pv207.pv207_project.OrderItem> item) {
+		this.item = item;
+	}
+
+	public Order(java.lang.Integer sum,
+			java.util.List<com.pv207.pv207_project.OrderItem> item) {
+		this.sum = sum;
+		this.item = item;
+	}
 
 }
